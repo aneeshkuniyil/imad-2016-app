1ad0b69b96d886138a2b1f14462db2e4394871f6
aneeshkuniyil <aneesh@sarayoo.info>
2016-09-19 21:18:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/aneesh.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send('Article one will be served here..')
+    res.sendFile(path.join(__dirname, 'ui', 'article.html'))
 });
 
 app.get('/article-two', function (req, res){
