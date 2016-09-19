4d74654ca1c0a808879ca4f9e655759a3fc9405c
aneeshkuniyil <aneesh@sarayoo.info>
2016-09-19 15:55:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ app.get('/ui/aneesh.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aneesh.png'));
 });
 
+app.get('/ui/facebook.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'facebook.png'));
+});
+
+app.get('/ui/twitter.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'twitter.png'));
+});
+
+app.get('/ui/youtube.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'youtube.png'));
+});
+
+app.get('/ui/linkedin.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'linkedin.png'));
+});
+
+app.get('/ui/googleplus.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'googleplus.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
