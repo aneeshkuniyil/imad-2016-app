ac8f79dd5266808eb588acbff3a413ba1a598da8
aneeshkuniyil <aneesh@sarayoo.info>
2016-09-20 21:43:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,48 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+  title : 'Article One',
+  heading : 'Article One - HTML <style> tag for writing CSS inside the HTML',
+  date : '19 Sept 2016',
+  content : `
+  <p>The <b>HTML &ltstyle&gt element</b> contains style information for a document, or part of a document. By default, the style instructions written inside that element are expected to be <a href = "https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</a>.</p>`
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+
+    var htmlTemplate = `
+      <html>
+        <head>
+            <title>${title}</title>
+            <meta name =viexmport content = "width-devise-width" initial-scale="1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    	<body>
+    	    <div class="container">
+        	<div>
+        	        <a href = "/">Home</a>
+                    <a href="/article-two">Article Two</a>
+                    <a href="/article-three">Article Three</a>
+        	 </div>
+        	 <hr/>
+        	 <h1> ${heading} </h1>
+            <div>
+        	    ${date}
+        	</div>
+        	<div>
+                ${content}   
+            </div>
+    	</body>
+    </html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +60,8 @@ app.get('/ui/aneesh.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
+   // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res){
